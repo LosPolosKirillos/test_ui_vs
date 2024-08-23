385688793fc14603fe1164f1597c6edad1a74186
Кирилл Лиховидов <cosmolihov3@gmail.com>
2024-08-23 21:49:35 +0300

    test commit

---
+++ text.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_start_firstCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Просто print()?");
+        }
+    }
+}
